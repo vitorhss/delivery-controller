97717987e8a539ee831bf7605e3e97ae6b8e7b2a
Vitor Souza <vitorrhss@gmail.com>
2017-02-10 18:14:45 -0200

    Criando os packages base para o projeto

--- src/main/java/br/com/delivery/management/DeliveryControllerApplication.java
+++ src/main/java/br/com/delivery/management/DeliveryControllerApplication.java
@@ -1,4 +1,4 @@
-package br.com.delivery.controller;
+package br.com.delivery.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
