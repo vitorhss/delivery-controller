5403f8a32b1828605fb88246b4f1c9db6e2c7e2d
Vitor Souza <vitorrhss@gmail.com>
2017-02-14 10:11:35 -0200

    Pacote Form Model

---
+++ src/main/java/br/com/delivery/management/form/model/RotaFormModel.java
@@ -0,0 +1,61 @@
+package br.com.delivery.management.form.model;
+
+import javax.validation.constraints.NotNull;
+
+public class RotaFormModel {
+	@NotNull
+	private String nome;
+	
+	@NotNull
+	private String pontoInicial;
+	
+	@NotNull
+	private String pontoFinal;
+	
+	@NotNull
+	private double autonomiaCaminhao;
+	
+	@NotNull
+	private double valorKilometro;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getPontoInicial() {
+		return pontoInicial;
+	}
+
+	public void setPontoInicial(String pontoInicial) {
+		this.pontoInicial = pontoInicial;
+	}
+
+	public String getPontoFinal() {
+		return pontoFinal;
+	}
+
+	public void setPontoFinal(String pontoFinal) {
+		this.pontoFinal = pontoFinal;
+	}
+
+	public double getAutonomiaCaminhao() {
+		return autonomiaCaminhao;
+	}
+
+	public void setAutonomiaCaminhao(double autonomiaCaminhao) {
+		this.autonomiaCaminhao = autonomiaCaminhao;
+	}
+
+	public double getValorKilometro() {
+		return valorKilometro;
+	}
+
+	public void setValorKilometro(double valorKilometro) {
+		this.valorKilometro = valorKilometro;
+	}
+
+}
